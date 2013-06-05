2f748188030161b9f780d85081ce95bc568dbd04
itang <live.tang@gmail.com>
2013-06-05 13:57:00 +0800

    cleanup

--- gohttp.go
+++ gohttp.go
@@ -16,54 +16,45 @@ import (
 )
 
 var (
-	PORT    = 8080
-	WEBROOT = "."
+	port    = 8080
+	webroot = "."
 )
 
-type Server struct {
-	Port    int
-	Webroot string
-}
+var tmp = template.Must(template.New("index").Parse(html))
 
+const html = `
+<a href="{{.ParentURI}}"> {{.ParentURI}} </a> | <a href="{{.CurrentURI}}">{{.CurrentURI}}</a>
+<ul>
+   {{range .files}}
+      <li><a href="{{.URI}}">{{.Name}}
+      {{if .Size }}
+      <small>({{.Size}})</small>
+      {{end}}
+      </a></li>
+   {{end}}
+</ul>`
+
+////////////////////////////////////////////////////////////////////////////////////////////
 func init() {
 	wd, _ := os.Getwd()
-	log.Printf("current dir:%v", wd)
+	log.Printf("Current dir:%v", wd)
 	log.Printf("PathSeparator:%c", os.PathSeparator)
 
-	flag.IntVar(&PORT, "port", PORT, "the port (default is 8080)")
-	flag.StringVar(&WEBROOT, "webroot", wd, "Web root directory (default is current work directory)")
+	flag.IntVar(&port, "port", port, "The port (default is 8080)")
+	flag.StringVar(&webroot, "webroot", wd, "Web root directory (default is current work directory)")
 }
 
-func (server *Server) router() {
-	http.Handle("/", server)
+//////////////////////////////////////////////////////////////////////////////////////////
+type Server struct {
+	port    int
+	webroot string
 }
 
-func (server *Server) Start() {
-	log.Printf("port:%v", server.Port)
-	log.Printf("webroot:%v", server.Webroot)
-
-	server.router()
-
-	addr := fmt.Sprintf(":%v", server.Port)
-	fmt.Printf("start server at :%v\n", server.Port)
-	if err := http.ListenAndServe(addr, nil); err != nil {
-		fmt.Errorf("%v", err)
-	}
-}
-
-func (server *Server) requestURIToFilepath(uri string) (fullpath string, relpath string) {
-	unescapeIt, _ := url.QueryUnescape(uri)
-
-	relpath = unescapeIt
-	log.Printf("unescape uri:%v", relpath)
-
-	fullpath = filepath.Join(server.Webroot, relpath[1:])
-	//** trace
-	log.Printf("base path:%v\n", path.Base(fullpath))
-	log.Printf("dir path:%v\n", path.Dir(fullpath))
-	log.Printf("ext path:%v\n", path.Ext(fullpath))
-	//**
-	return
+type Item struct {
+	Name  string
+	Title string
+	URI   string
+	Size  int64
 }
 
 func checkError(err error) {
@@ -72,11 +63,71 @@ func checkError(err error) {
 	}
 }
 
-type Item struct {
-	Name  string
-	Title string
-	URI   string
-	Size  int64
+func (server *Server) Start() {
+	log.Printf("Port:%v", server.port)
+	log.Printf("Webroot:%v", server.webroot)
+
+	server.router()
+
+	addr := fmt.Sprintf(":%v", server.port)
+	fmt.Printf("Start server at :%v\n", server.port)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Errorf("%v", err)
+	}
+}
+
+func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Printf("<< Request from %v", req.RemoteAddr)
+
+	defer func() {
+		if err := recover(); err != nil {
+			http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
+		}
+	}()
+
+	server.handler(w, req)
+
+	log.Printf("End Request>>")
+}
+
+func (server *Server) router() {
+	http.Handle("/", server)
+}
+
+func (server *Server) handler(w http.ResponseWriter, req *http.Request) {
+	uri := req.RequestURI      // 请求的URI, 如http://localhost:8080/hello -> /hello
+	if uri == "/favicon.ico" { // 不处理
+		return
+	}
+
+	fullpath, relpath := server.requestURIToFilepath(uri)
+	log.Printf("\tTo Filepath:%v", fullpath)
+
+	file, err := os.Open(fullpath)
+	if err != nil || os.IsNotExist(err) { // 文件不存在
+		http.NotFound(w, req)
+	} else {
+		stat, _ := file.Stat()
+		if stat.IsDir() {
+			log.Printf("\tProcess Dir...")
+			server.processDir(w, file, fullpath, relpath)
+		} else {
+			log.Printf("\tSend File...")
+			server.sendFile(w, file, fullpath, relpath)
+		}
+	}
+}
+
+func (server *Server) requestURIToFilepath(uri string) (fullpath string, relpath string) {
+	unescapeIt, _ := url.QueryUnescape(uri)
+
+	relpath = unescapeIt
+	log.Printf("\tUnescape URI:%v", relpath)
+
+	fullpath = filepath.Join(server.webroot, relpath[1:])
+	//log.Printf("base path:%v, dir path:%v, ext path:%v\n", path.Base(fullpath), path.Dir(fullpath), path.Ext(fullpath))
+
+	return
 }
 
 func (server *Server) processDir(w http.ResponseWriter, dir *os.File, fullpath string, relpath string) {
@@ -114,61 +165,11 @@ func (server *Server) sendFile(w http.ResponseWriter, file *os.File, fullpath st
 	io.Copy(w, file)
 }
 
-func (server *Server) handler(w http.ResponseWriter, req *http.Request) {
-	uri := req.RequestURI      // 请求的URI, 如http://localhost:8080/hello -> /hello
-	if uri == "/favicon.ico" { // 不处理
-		return
-	}
-
-	fullpath, relpath := server.requestURIToFilepath(uri)
-	log.Printf("To Filepath:%v", fullpath)
-
-	file, err := os.Open(fullpath)
-	if err != nil || os.IsNotExist(err) { // 文件不存在
-		http.NotFound(w, req)
-	} else {
-		stat, _ := file.Stat()
-
-		log.Printf("%v is dir? %v\n", file.Name(), stat.IsDir())
-		if stat.IsDir() {
-			server.processDir(w, file, fullpath, relpath)
-		} else {
-			server.sendFile(w, file, fullpath, relpath)
-		}
-	}
-}
-
-func (server *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Printf("<< Request from %v", req.RemoteAddr)
-
-	defer func() {
-		if err := recover(); err != nil {
-			http.Error(w, fmt.Sprintf("%v", err), http.StatusInternalServerError)
-		}
-	}()
-
-	server.handler(w, req)
-
-	log.Printf(" End Request>>")
-}
+////////////////////////////////////////////////////////////////////////////////////////////////
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU()*2 - 1)
 	flag.Parse()
-	server := &Server{Port: PORT, Webroot: WEBROOT}
+	server := &Server{port: port, webroot: webroot}
 	server.Start()
 }
-
-var tmp = template.Must(template.New("index").Parse(html))
-
-const html = `
-<a href="{{.ParentURI}}"> {{.ParentURI}} </a> | <a href="{{.CurrentURI}}">{{.CurrentURI}}</a>
-<ul>
-   {{range .files}}
-      <li><a href="{{.URI}}">{{.Name}}
-      {{if .Size }}
-      <small>({{.Size}})</small>
-      {{end}}
-      </a></li>
-   {{end}}
-</ul>`
